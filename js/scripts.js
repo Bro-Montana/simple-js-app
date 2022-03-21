e872f3a44233f0d6e7476dd97faebf023408a70f
Bro-Montana <97376411+Bro-Montana@users.noreply.github.com>
2022-03-21 14:51:01 -0700

    Added 2 objects to pokemonList array

--- js/scripts.js
+++ js/scripts.js
@@ -3,5 +3,13 @@ let pokemonList = [
     name: 'Bulbasuar',
     height: 7,
     types: ['grass','poison']
+  }, {
+    name: 'Charizard',
+    height: 4,
+    types: ['fire', 'flying']
+  }, {
+    name: 'Squirtle',
+    height: 1,
+    types: ['water']
   }
 ];
