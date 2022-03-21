ff9eeaf4a187169cba74bdb7b3d8fb5187fb533e
Bro-Montana <97376411+Bro-Montana@users.noreply.github.com>
2022-03-21 15:18:09 -0700

    code to highlight special Pokémon in array

--- js/scripts.js
+++ js/scripts.js
@@ -13,3 +13,20 @@ let pokemonList = [
     types: ['water']
   }
 ];
+
+// Loop for list of pokemon and their attributes
+let pokemonList = [
+  {
+    name: 'Bulbasuar',
+    height: 7,
+    types: ['grass','poison']
+  }, {
+    name: 'Charizard',
+    height: 4,
+    types: ['fire', 'flying']
+  }, {
+    name: 'Squirtle',
+    height: 1,
+    types: ['water']
+  }
+];
