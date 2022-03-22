1a7fa3e9ca02636447617c8c26b52ed62110fe5c
Bro-Montana <97376411+Bro-Montana@users.noreply.github.com>
2022-03-22 11:10:01 -0700

    added missing 'for' loop conditional to print

--- js/scripts.js
+++ js/scripts.js
@@ -15,18 +15,11 @@ let pokemonList = [
 ];
 
 // Loop for list of pokemon and their attributes
-let pokemonList = [
-  {
-    name: 'Bulbasuar',
-    height: 7,
-    types: ['grass','poison']
-  }, {
-    name: 'Charizard',
-    height: 4,
-    types: ['fire', 'flying']
-  }, {
-    name: 'Squirtle',
-    height: 1,
-    types: ['water']
+for (let i=0; i < pokemonList.length; i++)
+    if (pokemonList[i].height > 4) {
+    document.write(pokemonList[i].name + " height: " + pokemonList[i].height + " - Wow, you are pretty tall!");
+  } else if (pokemonList[i].height < 4) {
+    document.write(pokemonList[i].name + " height: " + pokemonList[i].height + " - Aww, you are short!")
+  } else {
+    document.write(pokemonList[i].name + " height: " + pokemonList[i].height + " - You can be short or tall!")
   }
-];
